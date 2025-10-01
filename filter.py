1617df54b7c96517f3cab9933ecfa2a27b072cdb
Nurtal <nathan.foulquier.pro@gmail.com>
2025-10-01 11:27:20 +0200

    update filer

--- filter.py
+++ filter.py
@@ -1,13 +1,35 @@
 import polars as pl
+import datetime as dt
 
-def filter_date(df):
-    """ """
 
-    print(df)
+def filter_date(df:pl.DataFrame, date_min:str, date_max:str) -> pl.DataFrame:
+    """Filter dataframe according to publication date
+
+    Args:
+        - df (pl.DataFrame) : polars dataframe containing article
+        - date_min (str) : min date fo filter, formated as d/m/Y
+        - date_max (str) : max date fo filter, formated as d/m/Y
+
+    Returns:
+        - (pl.DataFrame) : filtered dataframe
+    
+    """
+
+    # preprocess dates
+    date_min = dt.datetime.strptime(date_min, "%d/%m/%Y").date()
+    date_max = dt.datetime.strptime(date_max, "%d/%m/%Y").date()
+
+    # apply filter
+    df = df.filter((pl.col("PublicationDate") >= date_min) & (pl.col("PublicationDate") <= date_max) )
+
+    # return df
+    return df
+
 
 
 if __name__ == "__main__":
 
     df = pl.read_parquet("/tmp/test.parquet")
-    filter_date(df)
+    df = filter_date(df, "12/09/2025", "17/09/2025")
+    print(df)
     
