a5b570cdaa5be8cf4264f51d60d0f62ca92d24cc
Nurtal <nathan.foulquier.pro@gmail.com>
2025-10-01 14:08:29 +0200

    add keyword search

--- filter.py
+++ filter.py
@@ -26,10 +26,32 @@ def filter_date(df:pl.DataFrame, date_min:str, date_max:str) -> pl.DataFrame:
     return df
 
 
+def filter_keyword(df, target_word):
+    """Filter article with targetçwords present either in title, abstract or keywords
+
+    Args:
+        - df (pl.DataFrame) : polars dataframe containing article
+        - target_word (str) : word to search
+
+    Returns:
+        - (pl.DataFrame) : filtered dataframe
+    
+    """
+
+    # perform filter
+    df = df.filter(
+        (pl.col("Title").str.contains(target_word, literal=False)) |
+        (pl.col("Abstract").str.contains(target_word, literal=False)) |
+        (pl.col("Keywords").str.contains(target_word, literal=False))
+    )
+
+    return df
+
 
 if __name__ == "__main__":
 
     df = pl.read_parquet("/tmp/test.parquet")
-    df = filter_date(df, "12/09/2025", "17/09/2025")
+    # df = filter_date(df, "12/09/2025", "17/09/2025")
+    df = filter_keyword(df, "cancer")
     print(df)
     
