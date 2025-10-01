51ada8ea6d9e2d3ce638842e0f6d5d9c06fdfa79
Nurtal <nathan.foulquier.pro@gmail.com>
2025-10-01 16:49:56 +0200

    init main

--- pub2csv.py
+++ pub2csv.py
@@ -1,16 +1,23 @@
 import download
 import parser
+import filter
 import glob
 import polars as pl
 
-def run(date_min, date_max):
-    """ """
+def run(date_min:str, date_max:str, result_file:str) -> None:
+    """Run the download, parsing and filter of the articles
+
+    Args:
+        - date_min (str) : min date for article publication in the format d/m/Y
+        - date_max (str) : max date for article publication in the format d/m/Y        
+        - result_file (str) : path to the result (parquet file)
+    
+    """
 
     # parameters
     ncbi_server_address = "ftp.ncbi.nlm.nih.gov"
     pubmed_emplacement = "/pubmed/updatefiles/"
     output_folder = "/tmp/pub2csv"
-    result_file = "/tmp/test.parquet"
 
     # download files
     target_file_list = download.get_list_of_pubmed_files(ncbi_server_address, pubmed_emplacement)
@@ -25,10 +32,15 @@ def run(date_min, date_max):
         df = parser.clean_df(df)
         df_list.append(df)
     df = pl.concat(df_list)
+
+    # filter on date
+    filter.filter_date(df, date_min, date_max)
+    
+    # save
     df.write_parquet(result_file)
 
 
     
 if __name__ == "__main__":
 
-    run("12/09/2025", "14/09/2025")
+    run("12/09/2025", "14/09/2025", "/tmp/machin.parquet")
