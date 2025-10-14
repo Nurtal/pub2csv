5e5248b12c65eb70cb4a9d0993667c0cf7be690e
Nurtal <nathan.foulquier.pro@gmail.com>
2025-10-14 16:36:30 +0200

    get size and last update date close #28

--- src/pub2csv/download.py
+++ src/pub2csv/download.py
@@ -105,12 +105,33 @@ def download_file_list(file_list:list, ncbi_server_address:str, pubmed_emplaceme
     
 
 
-def get_files_metadata(ncbi_server_address:str, pubmed_emplacement:str):
+def get_files_metadata(ncbi_server_address:str, pubmed_emplacement:str) -> dict:
     """Extract size and last date of update of each files in pubmed_emplacement
+    
+    Args:
+        - ncbi_server_address (str) : ftp server adress
+        - pubmed_emplacement (str) : folder where to fond the files on the ftp server
 
-    TODO : Finish last date update computation
+    Returns:
+        - (dict) : filename to metadata
     
     """
+
+    # utils
+    month_to_num = {
+        "Jan": "01",
+        "Feb": "02",
+        "Mar": "03",
+        "Apr": "04",
+        "May": "05",
+        "Jun": "06",
+        "Jul": "07",
+        "Aug": "08",
+        "Sep": "09",
+        "Oct": "10",
+        "Nov": "11",
+        "Dec": "12"
+    }
 
     # connect to the NCBI server
     ftp = FTP(ncbi_server_address)
@@ -132,15 +153,30 @@ def get_files_metadata(ncbi_server_address:str, pubmed_emplacement:str):
         month = line[5]
         day = line[6]
 
-        print(line)
+        # try to craft date
+        extention = filename.split(".")[-1]
+        if extention in ('md5', 'gz'):
+            year = filename[6:8]
+            year = f"20{year}"
 
+            # cast month to int
+            try:
+                month = month_to_num[month]
+            except:
+                month = None
+        else:
+            year = None
+
+        # assemble update date
+        if None not in [year, month]:
+            update_date = f"{day}/{month}/{year}"
+        else:
+            update_date = None
+        
         # save info
-        file_to_data[filename] = {"SIZE":size}
+        file_to_data[filename] = {"SIZE":size,"UPDATED":update_date}
 
-
-    # print(file_to_data)
-
-
+    return file_to_data
 
 
 if __name__ == "__main__":
@@ -153,5 +189,6 @@ if __name__ == "__main__":
     # m = get_files_between_date(m, "12/09/2025", "25/09/2025")
     # download_file_list(m, ncbi_server_address, pubmed_emplacement, "/tmp/pub2csv")
 
-    get_files_metadata(ncbi_server_address, pubmed_emplacement)
+    m = get_files_metadata(ncbi_server_address, pubmed_emplacement)
+    print(m)
     
