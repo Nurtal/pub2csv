5be0c79b55f714cf75091b3cc7196221ebc29878
Nurtal <nathan.foulquier.pro@gmail.com>
2025-10-22 15:02:16 +0200

    patch and deal with updates func close #38

--- src/pub2csv/pub2csv.py
+++ src/pub2csv/pub2csv.py
@@ -16,6 +16,7 @@ def get_baseline_data(output_folder:str, max_retries:int, override:bool) -> None
     Args:
         - output_folder (str) : name of the folder to store downloaded files parsed as parquet
         - max_retries (int) : number of attempts authorized to download files
+        - override (bool) : if True clean output folder if exist, if False just download the missing files from output folder
     """
 
     # parameters
@@ -27,10 +28,10 @@ def get_baseline_data(output_folder:str, max_retries:int, override:bool) -> None
         shutil.rmtree(output_folder)
 
     # init output folder
+    dl_files = []
     if not os.path.isdir(output_folder):
         os.mkdir(output_folder)
     else:
-        dl_files = []
         processed_files = glob.glob(f"{output_folder}/*.parquet")
         for pf in processed_files:
             dl_files.append(pf.replace(".parquet", ".xml.gz").split("/")[-1])
@@ -79,7 +80,83 @@ def get_baseline_data(output_folder:str, max_retries:int, override:bool) -> None
     # display coverage
     coverage = float( (len(all_files) - len(to_retry)) / len(all_files) ) *100.0
     print(f"[*] Extract {coverage} % of baseline articles")
+
+
+
+def get_updatefiles_data(output_folder:str, max_retries:int, override:bool) -> None:
+    """Download the content of updatefiles pubmed folder into output folder
+    Can take a while, a lot of files to download
+
+    Args:
+        - output_folder (str) : name of the folder to store downloaded files parsed as parquet
+        - max_retries (int) : number of attempts authorized to download files
+        - override (bool) : if True clean output folder if exist, if False just download the missing files from output folder
+    """
+
+    # parameters
+    ncbi_server_address = "ftp.ncbi.nlm.nih.gov"
+    folder_location = "/pubmed/updatefiles/"
+
+    # clean output folder if it already exist and override is set to True
+    if override and os.path.isidir(output_folder):
+        shutil.rmtree(output_folder)
+
+    # init output folder
+    dl_files = []
+    if not os.path.isdir(output_folder):
+        os.mkdir(output_folder)
+    else:
+        processed_files = glob.glob(f"{output_folder}/*.parquet")
+        for pf in processed_files:
+            dl_files.append(pf.replace(".parquet", ".xml.gz").split("/")[-1])
+
+    # check volume capacity
+    if not check_folder_capacity(output_folder, 16):
+        print(f"[!] Not enough space to write in folder {output_folder}")
+        return None
+
+    # create ftp connection
+    ftp = get_ftp_connection(ncbi_server_address, folder_location)
+
+    # get list of files to download
+    file_list = []
+    all_files = get_list_of_pubmed_files(ftp)
+    for af in all_files:
+        if af not in dl_files:
+            file_list.append(af)
+
+    # init loop parameter
+    to_retry = file_list
+    attempts = 0
+
+    # collect data
+    while to_retry and attempts < max_retries:
+        failed = []
+        for gz_file in tqdm(to_retry, desc=f"[Attempt {attempts+1}] Extracting Baseline Data"):
+            if download_and_check(gz_file, output_folder, ftp):
+                xml_to_parquet(f"{output_folder}/{gz_file}", f"{output_folder}/{gz_file.replace('.xml.gz', '.parquet')}", True)
+            else:
+                failed.append(gz_file)
+
+        # update loop parameter
+        to_retry = failed
+        attempts +=1
+
+    # display missing files
+    if to_retry:
+        print(f"[!]Failed to download the following files after {attempts} attempts:")
+        for gz_file in to_retry:
+            print(f"\t- {gz_file}")
+
+    # close ftp connection
+    ftp.close()
     
+    # display coverage
+    coverage = float( (len(all_files) - len(to_retry)) / len(all_files) ) *100.0
+    print(f"[*] Extract {coverage} % of baseline articles")
+
+
+
 
 def run(date_min:str, date_max:str, result_file:str) -> None:
     """Run the download, parsing and filter of the articles
@@ -121,4 +198,5 @@ def run(date_min:str, date_max:str, result_file:str) -> None:
 if __name__ == "__main__":
 
     # run("12/09/2025", "14/09/2025", "/tmp/machin.parquet")
-    get_baseline_data("/tmp/pubfetch2", 3, False)
+    # get_baseline_data("/tmp/pubfetch2", 3, False)
+    get_updatefiles_data("/tmp/pubfetch4", 3, False)
